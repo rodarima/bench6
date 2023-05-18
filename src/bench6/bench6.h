c543a5582c64efb5352695732f54f49f55579810
Rodrigo Arias <rodarima@gmail.com>
2023-05-18 19:43:48 +0200

    Add missing bench6.h file

---
+++ src/bench6/bench6.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2022 Barcelona Supercomputing Center (BSC)
+ * SPDX-License-Identifier: GPL-3.0-or-later */
+
+#ifndef BENCH6_H
+#define BENCH6_H
+
+#define UNUSED(x) (void)(x)
+
+double get_time(void);
+int get_ncpus(void);
+
+int bench6_creator(int argc, char *argv[]);
+int bench6_sched_get(int argc, char *argv[]);
+int bench6_sched_add(int argc, char *argv[]);
+int bench6_register_deps(int argc, char *argv[]);
+
+#endif /* BENCH6_H */
