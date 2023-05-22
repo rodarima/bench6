fd1282863e874345989064d44000c948f428d228
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-05-22 18:52:46 +0200

    Only search for Nanos6 and NODES in *_HOME vars

--- cmake/FindNanos6.cmake
+++ cmake/FindNanos6.cmake
@@ -1,8 +1,14 @@
 include(GNUInstallDirs)
 
-find_library(NANOS6_LIBRARY NAMES nanos6)
-find_path(NANOS6_INCLUDE_DIR nanos6.h)
-find_file(NANOS6_WRAPPER NAMES nanos6-main-wrapper.o PATH_SUFFIXES "lib")
+if(DEFINED ENV{NANOS6_HOME})
+  set(NANOS6_HOME "$ENV{NANOS6_HOME}")
+else()
+  message(STATUS "NANOS6_HOME not set, refusing to search")
+endif()
+
+find_library(NANOS6_LIBRARY NAMES nanos6 PATHS "${NANOS6_HOME}/lib" NO_DEFAULT_PATH)
+find_path(NANOS6_INCLUDE_DIR nanos6.h PATHS "${NANOS6_HOME}/include" NO_DEFAULT_PATH)
+find_file(NANOS6_WRAPPER NAMES nanos6-main-wrapper.o PATHS "${NANOS6_HOME}/lib" NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
 
