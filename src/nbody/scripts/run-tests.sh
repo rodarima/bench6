79358c74026c5ec6c72fb210017fa34f3e3d1960
Rodrigo Arias <rodrigo.arias@bsc.es>
2024-11-20 10:04:36 +0100

    Add simple nbody seq and omp
    The blocksize is hardcoded to 2048 for now.

---
+++ src/nbody/scripts/run-tests.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+function usage {
+	if [ "$#" -eq 1 ]; then
+		echo "Error: $1"
+	fi
+	echo "Usage: ./run-tests.sh [-p exec_path] [-np num_hybrid_procs] [-nt total_threads] [-s num_particles] [-t timesteps] [-h]"
+	exit 1
+}
+
+function getprogname {
+	local progarray=()
+	IFS='/' read -r -a progarray <<< "$1"
+	local index=$((${#progarray[@]} - 1))
+	echo "${progarray[$index]}"
+}
+
+# Default executables path
+path=$PWD
+
+# Default configuration
+nprocs=2
+nthreads=4
+
+# Default execution parameters
+size=$((16*1024))
+timesteps=20
+
+nargs=$#
+args=("$@")
+
+error=$(($nargs % 2))
+if [ "$error" -eq 1 ]; then
+	usage
+fi
+
+i=0
+while (("$i" < "$nargs")); do
+	opt=${args[$i]}
+	((i++))
+	val=${args[$i]}
+	((i++))
+
+	if [ "$opt" == "-p" ]; then
+		path=$val
+	elif [ "$opt" == "-np" ]; then
+		nprocs=$val
+	elif [ "$opt" == "-nt" ]; then
+		nthreads=$val
+	elif [ "$opt" == "-s" ]; then
+		size=$val
+	elif [ "$opt" == "-t" ]; then
+		timesteps=$val
+	else
+		usage "'$opt' is not a valid option!"
+	fi
+done
+
+nthreadsxproc=$((nthreads / nprocs))
+
+echo ---------------------------------
+echo TEST SUITE
+echo ---------------------------------
+
+echo "Configuration (Distributed versions):"
+echo "  $nthreads processes"
+echo "Configuration (Hybrid versions):"
+echo "  $nprocs processes"
+echo "  $nthreadsxproc threads per proc"
+echo "Execution parameters:"
+echo "  $size particles"
+echo "  $timesteps timesteps"
+echo "Getting all '*.exe' files from path '$path'..."
+
+# Get all executable files from the desired path
+programs=($path/*.exe)
+
+# Oss task for is only supported by "fifo" and "naive" schedulers
+export NANOS6_SCHEDULER=fifo
+export NANOS6=optimized
+
+logfile=.nbody_log
+
+rm -f $logfile
+
+total=0
+failed=0
+
+echo "Starting the verification..."
+echo ---------------------------------
+
+for prog in ${programs[*]}; do
+	# Check if it exists
+	if [ ! -f ${prog} ]; then
+		continue;
+	fi
+	total=$(($total + 1))
+
+	progname=$(getprogname $prog)
+
+	# Execution
+	if [[ $progname == *"_mpi."* ]]; then
+		mpiexec.hydra -n $nthreads $prog -p $size -t $timesteps -c &> $logfile
+		error=$?
+	elif [[ $progname == *"_mpi_ompss"* ]]; then
+		mpiexec.hydra -n $nprocs -bind-to hwthread:$nthreadsxproc $prog -p $size -t $timesteps -c &> $logfile
+		error=$?
+	else
+		$prog -p $size -t $timesteps -c &> $logfile
+		error=$?
+	fi
+
+	# Check the return value of the program
+	if [ "$error" -ne 0 ]; then
+		failed=$(($failed + 1))
+		echo $progname FAILED
+		rm -f $logfile
+		continue;
+	fi
+
+	# Result checking
+	grep -Fxq 'Result validation: OK' $logfile
+	error=$?
+	if [ "$error" -eq 0 ]; then
+		echo $progname PASSED
+	else
+		failed=$(($failed + 1))
+		echo $progname FAILED
+	fi
+	rm -f $logfile
+done
+
+echo ---------------------------------
+if [ $failed -eq 0 ]; then
+	echo SUMMARY: ALL TESTS PASSED
+else
+	echo SUMMARY: ${failed}/${total} TESTS FAILED
+fi
+echo ---------------------------------
+
+rm -f $logfile
---
+++ src/nbody/src/common/common.h
@@ -0,0 +1,74 @@
+//
+// This file is part of NBody and is licensed under the terms contained
+// in the LICENSE file.
+//
+// Copyright (C) 2021 Barcelona Supercomputing Center (BSC)
+//
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stddef.h>
+
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 2048
+#endif
+
+// BIGO definition
+#ifndef BIGO
+#define BIGO N2
+#define _BIGO_N2
+#endif
+
+#define PART 1024
+#define PAGE_SIZE 4096
+
+#define TOLERATED_ERROR 0.0008
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#define LOG2(a) (31-__builtin_clz((a)))
+#define MOD(a, b)  ((a) < 0 ? ((((a) % (b)) + (b)) % (b)) : ((a) % (b)))
+#define ROUNDUP(x, y) ((((x) + (y - 1)) / y) * y)
+#define STRINGIFY(s) #s
+#define TOSTRING(s) STRINGIFY(s)
+#define CALCULATE_FORCES(s) calculate_forces_##s
+#define XCALCULATE_FORCES(s) CALCULATE_FORCES(s)
+#define calculate_forces XCALCULATE_FORCES(BIGO)
+
+static const float gravitational_constant   = 6.6726e-11f; /* N(m/kg)2 */
+static const float default_domain_size_x    = 1.0e+10; /* m  */
+static const float default_domain_size_y    = 1.0e+10; /* m  */
+static const float default_domain_size_z    = 1.0e+10; /* m  */
+static const float default_mass_maximum     = 1.0e+28; /* kg */
+static const float default_time_interval    = 1.0e+0;  /* s  */
+static const int   default_seed             = 12345;
+static const char* default_name             = "data/nbody";
+static const int   default_num_particles    = 0;
+static const int   default_timesteps        = 0;
+static const int   default_save_result      = 0;
+static const int   default_check_result     = 0;
+static const int   default_force_generation = 0;
+
+typedef struct {
+	float domain_size_x;
+	float domain_size_y;
+	float domain_size_z;
+	float mass_maximum;
+	float time_interval;
+	int seed;
+	const char* name;
+	int num_particles;
+	int num_blocks;
+	int timesteps;
+	int save_result;
+	int check_result;
+	int force_generation;
+} nbody_conf_t;
+
+nbody_conf_t nbody_get_conf(int argc, char **argv);
+double nbody_compute_throughput(int num_particles, int timesteps, double elapsed_time);
+void * nbody_alloc(size_t size);
+double get_time(void);
+
+#endif // COMMON_H
