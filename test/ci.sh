698b22678ced79a75c505370d7f32c3ee384d445
Rodrigo Arias <rodrigo.arias@bsc.es>
2024-11-29 16:00:22 +0100

    Run in Hut for now

--- test/ci.sh
+++ test/ci.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-repo="$CI_PROJECT_URL"
-branch="$CI_COMMIT_REF_NAME"
-commit="$CI_COMMIT_SHA"
-url="$repo?ref=$branch&rev=$commit"
+# Running in owl doesn't work yet
+#repo="$CI_PROJECT_URL"
+#branch="$CI_COMMIT_REF_NAME"
+#commit="$CI_COMMIT_SHA"
+#url="$repo?ref=$branch&rev=$commit"
+#srun --exclusive nix develop "$url" -c '$bench6src/test/bench.sh'
 
-srun --exclusive nix develop "$url" -c '$bench6src/test/bench.sh'
+nix develop -c test/bench.sh
