3cf304cda8b09a12abe50ea07174a0c03c221121
Rodrigo Arias <rodrigo.arias@bsc.es>
2024-12-04 12:48:56 +0100

    Only run miniapps once for now

--- test/bench.sh
+++ test/bench.sh
@@ -3,9 +3,11 @@
 set -e
 set -x
 
-bigotes b6_heat_nanos6 -s 2048 -t 10 -b 64
-bigotes b6_nbody_omp -f -b 512
-bigotes b6_nbody_ompv -f -b 512
-bigotes b6_miniamr_mpi_omp
-bigotes b6_nqueens_nanos6
-bigotes b6_strassen_nanos6
+#B=bigotes
+
+$B b6_heat_nanos6 -s 2048 -t 10 -b 64
+$B b6_nbody_omp -f -b 512
+$B b6_nbody_ompv -f -b 512
+$B b6_miniamr_mpi_omp
+$B b6_nqueens_nanos6
+$B b6_strassen_nanos6
