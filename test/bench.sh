f49c80ff95d1eee1c069e96f8b223e5d77fb4101
Rodrigo Arias <rodrigo.arias@bsc.es>
2024-12-05 10:50:50 +0100

    Add matmul to CI

--- test/bench.sh
+++ test/bench.sh
@@ -14,6 +14,7 @@ $B b6_strassen_nanos6
 $B b6_cholesky_nanos6
 $B b6_multisaxpy_nanos6
 $B b6_streaming_tampi_nanos6 -s 8096 -b 1024 -t 2000
+$B b6_matmul_itampi_nanos6 1024 2 512 0
 
 # Output not compatible with bigotes
 b6_tsunampi_tampi -r 10
