269b66d05d65c65f29697d5d39cc80d1355a8309
Antoni Navarro <anavarr1@bsc.es>
2026-02-18 16:35:07 +0100

    Remove unused TAMPI_VERSION from FindTampi.cmake

--- cmake/FindTampi.cmake
+++ cmake/FindTampi.cmake
@@ -16,25 +16,6 @@ if(TARGET Tampi::tampi-c)
   return()
 endif()
 
-# Parse TAMPI version from the TAMPI_Decl.h file
-file(STRINGS "${TAMPI_INCLUDE_DIR}/TAMPI_Decl.h" version-major-line
-  REGEX "#define TAMPI_VERSION_MAJOR .*")
-file(STRINGS "${TAMPI_INCLUDE_DIR}/TAMPI_Decl.h" version-minor-line
-  REGEX "#define TAMPI_VERSION_MINOR .*")
-
-if (NOT version-major-line OR NOT version-minor-line)
-  message(FATAL_ERROR "Cannot find TAMPI version lines")
-endif()
-
-string(REGEX REPLACE "^#define[ \t]+TAMPI_VERSION_MAJOR[ \t]+([0-9]+)$" "\\1"
-  TAMPI_VERSION_MAJOR ${version-major-line})
-string(REGEX REPLACE "^#define[ \t]+TAMPI_VERSION_MINOR[ \t]+([0-9]+)$" "\\1"
-  TAMPI_VERSION_MINOR ${version-minor-line})
-
-set(TAMPI_VERSION ${TAMPI_VERSION_MAJOR}.${TAMPI_VERSION_MINOR} CACHE STRING "TAMPI Version")
-
-message(STATUS "Found TAMPI version ${TAMPI_VERSION}")
-
 add_library(Tampi::tampi-c SHARED IMPORTED)
 set_target_properties(Tampi::tampi-c PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES "${TAMPI_INCLUDE_DIR}"
